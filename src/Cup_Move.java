fba87364001002a41ca57f6d37637224182c6264
KYUNGWON JUNG <103038606+kw99j10@users.noreply.github.com>
2022-08-20 14:00:06 +0900

    cup_move

--- src/Cup_Move.java
+++ src/Cup_Move.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
+public class Cup_Move {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
