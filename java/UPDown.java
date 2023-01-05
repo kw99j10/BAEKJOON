81e293e756dfa08eeea7e8ca5e7ec74c02c54753
KYUNGWON JUNG <103038606+kw99j10@users.noreply.github.com>
2023-01-05 19:52:34 +0900

    Update UPDown.java

--- java/UPDown.java
+++ java/UPDown.java
@@ -16,8 +16,8 @@ public class UPDown {
         int b = Integer.parseInt(st.nextToken()); //밤에 b 미터 떨어짐
         int k = Integer.parseInt(st.nextToken()); //총 올라가야 하는 거리
 
-        int distance=a-b; //하루동안 올라갈 수 있는 거리
-        int sum=k-b; //k까지 도달해야 하는 거리의 합
+        int distance=a-b; //하루동안 올라갈 수 있는 거리(낮에 a미터 올라간 만큼 b미터 밤에 떨어짐)
+        int sum=k-b; //k까지 도달해야 하는 거리의 합(b를 빼는 이유는 도달하기 직전에는 b만큼 미끄러지지 않기 때문)
 
         int days; //걸리는 일 수
         if (sum%distance==0)
