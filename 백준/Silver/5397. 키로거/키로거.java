a324faea386228ed667f5184e9a8fe06d0ac09ca
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-05-02 00:35:05 +0900

    [Silver II] Title: 키로거, Time: 936 ms, Memory: 281656 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < testCase; t++) {
-            int idx = 0;
             String s = br.readLine();
             LinkedList<Character> lists = new LinkedList<>();
+            ListIterator<Character> list = lists.listIterator();
             for (char c : s.toCharArray()) {
-               
+
                 if (c == '<') {
-                    if (idx > 0) {
-                        idx--;
+                    if (list.hasPrevious()) {
+                        list.previous();
                     }
                 } else if (c == '>') {
-                    if (lists.size() > idx) {
-                        idx++;
+                    if (list.hasNext()) {
+                        list.next();
                     }
                 } else if (c == '-') {
-                    if (idx > 0) {
-                        lists.remove(idx - 1);
-                        idx--;
+                    if (list.hasPrevious()) {
+                        list.previous();
+                        list.remove();
+
                     }
                 } else {
-                    lists.add(idx, c);
-                    idx++;
+                    list.add(c);
                 }
             }
-            for (Character list : lists) {
-                sb.append(list);
+            for (Character c : lists) {
+                sb.append(c);
             }
             sb.append("\n");
         }
