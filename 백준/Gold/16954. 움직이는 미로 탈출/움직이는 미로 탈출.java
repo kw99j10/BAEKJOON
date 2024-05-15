336e21679dfc5cdbca4943a87e7685b041a027f8
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-05-16 03:05:02 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 
+//움직이는 미로 탈출
 public class Main {
     static char[][] chess;
 
@@ -23,45 +24,46 @@ public class Main {
 
     static int bfs() {
         ArrayDeque<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{7, 0, 0}); //가장 왼쪽 아랫칸에서 시작
+        queue.add(new int[]{7, 0}); //가장 왼쪽 아랫칸에서 시작
 
-        int time = 0;
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int size = queue.size();
+            boolean[][] visit = new boolean[8][8];
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int x = current[0];
+                int y = current[1];
 
-            if (current[2] != time) {
-                //벽이 아래로 움직임
-                for (int i = 7; i >= 0; i--) {
-                    for (int j = 0; j < 8; j++) {
-                        if (chess[i][j] == '#') {
-                            if (i < 7) {
-                                chess[i + 1][j] = '#';
-                            }
-                            chess[i][j] = '.';
-                        }
-                    }
-                }
-                time++;
-            }
-
-            if (chess[x][y] == '#') {
-                continue;
-            }
-
-            if (x == 0 && y == 7) {
-                return 1;
-            }
-
-            for (int d = 0; d < 9; d++) {
-                int nx = x + move[d][0];
-                int ny = y + move[d][1];
-
-                if (nx < 0 || nx >= 8 || ny < 0 || ny >= 8 || chess[nx][ny] == '#') {
+                if (chess[x][y] == '#') {
                     continue;
                 }
-                queue.add(new int[]{nx, ny, current[2] + 1});
+
+                if (x == 0 && y == 7) {
+                    return 1;
+                }
+
+                for (int d = 0; d < 9; d++) {
+                    int nx = x + move[d][0];
+                    int ny = y + move[d][1];
+
+                    if (nx < 0 || nx >= 8 || ny < 0 || ny >= 8 || visit[nx][ny] || chess[nx][ny] == '#') {
+                        continue;
+                    }
+                    visit[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+
+            //욱제가 움직인 후 벽이 아래로 움직임
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < 8; j++) {
+                    if (chess[i][j] == '#') {
+                        if (i < 7) {
+                            chess[i + 1][j] = '#';
+                        }
+                        chess[i][j] = '.';
+                    }
+                }
             }
         }
         return 0;
