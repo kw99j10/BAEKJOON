d3acee52ae3aa0ed660bb2253ccdef33b75ede0d
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-06-12 23:13:30 +0900

    [Gold V] Title: 내려가기, Time: 344 ms, Memory: 50804 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 2096 내려가기
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][3];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+        int[][] dp = new int[n][3];
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]) + arr[i][0];
+            dp[i][1] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1], dp[i - 1][2])) + arr[i][1];
+            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]) + arr[i][2];
+        }
+        int max = Math.max(dp[n - 1][0], Math.max(dp[n - 1][1], dp[n - 1][2]));
+
+        dp = new int[n][3];
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], Math.min(dp[i - 1][1], dp[i - 1][2])) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][2];
+        }
+        int min = Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
+        System.out.println(max + " " + min);
+    }
+}
