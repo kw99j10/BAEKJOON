5007c252d8a7bf1d3482200222fda00138e765e9
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-06-19 17:56:41 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 444 ms, Memory: 59176 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 //3015 오아시스 재결합
 public class Main {
@@ -11,35 +9,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> h = new HashMap<>(); // 서로 볼 수 있는 쌍
-        long count = 0; //쌍의 수
+        Stack<int[]> st = new Stack<>(); // 사람 인덱스, 볼 수 있는 쌍
+        long count = 0;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            if (!h.containsKey(num)) {
-                h.put(num, 0);
-            }
+            int num = Integer.parseInt(br.readLine());
+            int[] current = new int[]{num, 1};
 
-            while (!stack.isEmpty()) {
-                if (stack.peek() > num) {
-                    count++; // 앞 사람 > 현재 사람: 앞 사람이 볼 수 있음 => +1
-                    break;
-                } else if (stack.peek() == num) {
-                    count += h.get(num) + 1; // 앞사람 = 현재 사람: 서로 볼 수 있음 => +1
-                    h.put(num, h.getOrDefault(num, 0) + 1);
-                    stack.pop();
-                } else {
-                    count++; // 현재 사람 > 앞 사람: 앞 사람은 그 뒤를 더이상 볼 수 없음 => pop && +1
-                    Integer before = stack.pop();
-                    if (h.get(before) != 0) {
-                        count += h.get(before); // 이전의 누적 쌍을 더함
-                        h.put(before, 0);
-                    }
+            while (!st.isEmpty() && num >= st.peek()[0]) {
+                int[] before = st.pop();
+                count += before[1];
+
+                if (before[0] == current[0]) {
+                    current[1] += before[1]; //키가 같을 경우
                 }
             }
-            stack.push(num);
+            if (!st.isEmpty()) {
+                count++;
+            }
+            st.push(current);
         }
         System.out.print(count);
     }
