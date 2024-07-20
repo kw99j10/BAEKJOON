37b627e6cd543bc96c00896fedb7caefaba99504
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-07-20 21:21:29 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 256 ms, Memory: 38072 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -9,35 +9,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
 
+        int[] arr = new int[n]; //주어진 수열
+        int[] count = new int[100001]; //같은 정수의 개수
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] count = new int[200001]; // 나온 정수의 개수
 
-        int max = 0; // 최장 연속 부분 수열 길이
-        int cnt = 0; // 현재 수열 길이
+        int max = 0;
+        int cnt = 0;
         int left = 0;
         int right = 0;
 
-        while (right < n) {
-            count[arr[right]]++;
-            cnt++;
-
-            while (count[arr[right]] > k) {
+        while (left <= right && right < n) {
+            if (count[arr[right]] >= k) {
                 count[arr[left]]--;
                 left++;
                 cnt--;
+            } else {
+                count[arr[right]]++;
+                right++;
+                cnt++;
             }
-
             max = Math.max(max, cnt);
-            right++;
         }
+
         System.out.println(max);
     }
 }
