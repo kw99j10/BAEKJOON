ba608347037ff2bb47fd07dfa5f9bb58e756aba0
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-07-25 22:17:22 +0900

    [Silver I] Title: 부등호, Time: 132 ms, Memory: 25664 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -2,65 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-//2529 부등호
+// 2529 부등호
 public class Main {
-    static String[] num;
-    static String[] str;
-    static boolean[] visit;
-    static ArrayList<String> list;
     static int n;
+    static char[] chr;
+    static boolean[] visit = new boolean[10];
+    static ArrayList<String> lists = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
-        str = new String[n];
+        chr = new char[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            str[i] = st.nextToken();
+            chr[i] = st.nextToken().charAt(0);
         }
 
-        num = new String[10];
-        visit = new boolean[10];
         for (int i = 0; i < 10; i++) {
-            num[i] = String.valueOf(i);
+            visit[i] = true;
+            backtracking(String.valueOf(i), i, 0); //문자열, 인덱스, 부등호 인덱스
+            visit[i] = false;
         }
 
-        list = new ArrayList<>();
-        backtracking("", 0); //부등호 관계 조합
-
-        // 최대 정수 && 최소 정수
-        Collections.sort(list);
-        System.out.println(list.get(list.size() - 1));
-        System.out.println(list.get(0));
+        System.out.println(lists.get(lists.size() - 1));
+        System.out.println(lists.get(0));
     }
 
-    static void backtracking(String result, int idx) {
-        if (idx == n + 1) {
-            list.add(result);
+    static void backtracking(String result, int idx, int cnt) {
+        if (cnt == n) {
+            lists.add(result);
             return;
         }
 
-        // 0~9 경우의 수 완탐
         for (int i = 0; i < 10; i++) {
             if (!visit[i]) {
-                if (idx == 0 || isPossible(Integer.parseInt(String.valueOf(result.charAt(idx - 1))), Integer.parseInt(num[i]), str[idx - 1])) {
+                if ((chr[cnt] == '<' && idx < i) || (chr[cnt] == '>' && idx > i)) {
                     visit[i] = true;
-                    backtracking(result + i, idx + 1);
+                    backtracking(result + i, i, cnt + 1);
                     visit[i] = false;
                 }
             }
-        }
-    }
-
-    static boolean isPossible(int a, int b, String c) {
-        if (c.equals(">")) {
-            return a >= b;
-        }
-        else{
-            return a <= b;
         }
     }
 }
