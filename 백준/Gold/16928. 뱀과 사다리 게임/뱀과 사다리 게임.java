273234ce91bbab1cf861c427630c51573a98dab0
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-08-30 11:16:09 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -2,14 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 // 16928 뱀과 사다리 게임
 public class Main {
-    static int[] board = new int[101];
-    static HashMap<Integer, Integer> ladder;
-    static HashMap<Integer, Integer> snake;
+    static int[] board = new int[101]; // 보드판
+    static int[] count = new int[101]; // 주사위 굴리는 횟수
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,51 +15,30 @@ public class Main {
         int m = Integer.parseInt(st.nextToken()); // 뱀의 수
 
         for (int i = 1; i <= 100; i++) {
-            board[i] = i - 1;
+            board[i] = i;
         }
 
-        ladder = new HashMap<>(); // 사다리 정보
-        snake = new HashMap<>(); // 뱀 정보
+        // 사다리 + 뱀 정보
         for (int i = 0; i < n + m; i++) {
             st = new StringTokenizer(br.readLine());
-            if (i < n) {
-                ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            }else{
-                snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            }
+            board[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
         }
         bfs();
-        System.out.println(board[100]); // 100번 칸
+        System.out.println(count[100]); // 100번 칸
     }
     static void bfs() {
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        boolean[] visit = new boolean[101];
         queue.add(1);
 
         while (!queue.isEmpty()) {
             Integer current = queue.poll();
-
-            if (visit[current]) {
-                continue;
-            }
-
-            visit[current] = true;
             for (int i = 1; i <= 6; i++) {
                 if (current + i > 100) {
-                    break; // 100번 칸을 넘어 이동 불가
+                    continue;
                 }
-
-                if (ladder.containsKey(current + i)) {
-                    queue.add(ladder.get(current + i));
-                    board[ladder.get(current + i)] = Math.min(board[ladder.get(current + i)], board[current] + 1);
-                }
-                else if (snake.containsKey(current + i)) {
-                    queue.add(snake.get(current + i));
-                    board[snake.get(current + i)] = Math.min(board[snake.get(current + i)], board[current] + 1);
-                }
-                else{
-                    queue.add(current + i);
-                    board[current + i] = Math.min(board[current + i], board[current] + 1);
+                if (count[board[current + i]] == 0) {
+                    queue.add(board[current + i]);
+                    count[board[current + i]] = count[current] + 1;
                 }
             }
         }
