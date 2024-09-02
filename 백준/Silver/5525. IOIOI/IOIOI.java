94f93d770996d452204e95559c657f9a7cf6f707
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-09-02 23:12:49 +0900

    [Silver I] Title: IOIOI, Time: 152 ms, Memory: 20656 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,29 +6,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); //Pn
+        int n = Integer.parseInt(br.readLine()); // Pn
         int m = Integer.parseInt(br.readLine()); // 문자열 길이
         String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < 2 * n + 1; i++) {
-            if (i % 2 == 0) {
-                sb.append("I");
-            }else{
-                sb.append("O");
-            }
-        }
 
         int cnt = 0;
-        StringBuilder sb2 = new StringBuilder();
-        for (int i = 0; i <= m - sb.length(); i++) {
-            for (int j = i; j < i + sb.length(); j++) {
-                sb2.append(s.charAt(j));
-            }
-            if (sb.toString().contentEquals(sb2)) {
-                cnt++; // 해당 문자열이 있다면 count
-            }else{
-                sb2.setLength(0); // 다르다면 sb 초기화
+        for (int i = 0; i <= m - (2 * n + 1); i++) {
+            if (s.charAt(i) == 'I') {
+                int tmp = 0;
+                while (m > i + 2 && s.charAt(i + 1) == 'O' && s.charAt(i + 2) == 'I') {
+                    tmp++;
+                    i += 2;
+                    if (tmp == n) {
+                        tmp--; // Pn을 발견
+                        cnt++;
+                    }
+                }
             }
         }
         System.out.println(cnt);
