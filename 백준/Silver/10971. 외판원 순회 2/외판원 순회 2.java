d944b9de7c590fb84980f817c9fee710994d702d
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-10-27 23:29:22 +0900

    [Silver II] Title: 외판원 순회 2, Time: 88 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -6,44 +6,37 @@ import java.util.StringTokenizer;
 // 10971 외판원 순회 2
 public class Main {
     static int n, min = Integer.MAX_VALUE;
-    static int[][] circle;
+    static int[][] w;
     static boolean[] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        circle = new int[n][n];
-
+        w = new int[n][n];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                circle[i][j] = Integer.parseInt(st.nextToken());
+                w[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         visit = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            visit[i] = true;
-            backtracking(i, i, 0, 0); //idx, start, count, 비용
-            visit[i] = false;
-        }
+        visit[0] = true;
+        tsm(0, 0, 0, 0); //한 번의 순회로 최소 비용을 얻을 수 있음
         System.out.println(min);
     }
 
-    static void backtracking(int idx, int start, int cnt, int cost) {
-        if (cnt == n - 1) {
-            if (circle[start][idx] == 0) {
-                return;
-            }
-            cost += circle[start][idx];
-            min = Math.min(min, cost);
+    static void tsm(int idx, int start, int cnt, int sum) {
+        if (cnt == n - 1 && w[start][idx] != 0) {
+            sum += w[start][idx];
+            min = Math.min(min, sum);
             return;
         }
         for (int i = 0; i < n; i++) {
-            if (visit[i] || circle[start][i] == 0) {
+            if (visit[i] || w[start][i] == 0) {
                 continue;
             }
             visit[i] = true;
-            backtracking(idx, i, cnt + 1, cost + circle[start][i]); //시작지점 변경
+            tsm(idx , i, cnt + 1, sum + w[start][i]);
             visit[i] = false;
         }
     }
