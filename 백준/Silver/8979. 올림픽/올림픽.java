f4823d2e4226ad9ead34b5a4df8104438b659145
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-10-29 00:41:06 +0900

    [Silver V] Title: 올림픽, Time: 68 ms, Memory: 11544 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 // 8979 올림픽
@@ -17,13 +18,13 @@ public class Main {
         }
 
         public int compareTo(Medal o) {
-            if (o.gold == this.gold) {
-                if (o.silver == this.silver) {
-                    return o.bronze - this.bronze;
-                }
+            if (o.gold != this.gold) {
+                return o.gold - this.gold;
+            }
+            if (o.silver != this.silver) {
                 return o.silver - this.silver;
             }
-            return o.gold - this.gold;
+            return o.bronze - this.bronze;
         }
     }
 
@@ -31,26 +32,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()); // 등수를 알고싶은 국가
 
-        PriorityQueue<Medal> queue = new PriorityQueue<>();
+        ArrayList<Medal> lists = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
             int gold = Integer.parseInt(st.nextToken());
             int silver = Integer.parseInt(st.nextToken());
             int bronze = Integer.parseInt(st.nextToken());
-            queue.add(new Medal(num, gold, silver, bronze));
+            lists.add(new Medal(num, gold, silver, bronze));
         }
+        Collections.sort(lists); // 조건 순 정렬
 
-        int cnt = 0;
-        while (true) {
-            if (!queue.isEmpty() && cnt == k - 1) {
-                System.out.println(queue.peek().num);
-                break;
+        int rank = 1;
+        int diff = 0;
+        int[] ranks = new int[n + 1];
+        ranks[lists.get(0).num] = rank;
+        for (int i = 1; i < n; i++) {
+            Medal before = lists.get(i - 1);
+            Medal current = lists.get(i);
+
+            //공동
+            if (before.gold == current.gold && before.silver == current.silver && before.bronze == current.bronze) {
+                ranks[current.num] = rank;
+                diff++;
+            } else {
+                rank += 1;
+                ranks[current.num] = rank + diff;
+                diff = 0;
             }
-            queue.poll();
-            cnt++;
         }
+        
+        System.out.println(ranks[k]);
     }
 }
