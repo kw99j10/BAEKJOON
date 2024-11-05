f6549ccd2906becb8bdf2a16d855aeaac6917154
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-11-05 19:40:47 +0900

    [Gold II] Title: 컵라면, Time: 964 ms, Memory: 74748 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] cup = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            cup[i][0]= Integer.parseInt(st.nextToken());
+            cup[i][1]= Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cup, Comparator.comparingInt(o -> o[0]));
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            queue.add(cup[i][1]);
+            if (!queue.isEmpty() && queue.size() > cup[i][0]) {
+                queue.poll();
+            }
+        }
+
+        int sum = 0;
+        for (Integer i : queue) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
