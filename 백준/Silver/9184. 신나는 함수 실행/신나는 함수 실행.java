016f4be87e1cf4c748115c4441b91f890aeee4ec
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-11-10 11:55:22 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 208 ms, Memory: 28620 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -5,7 +5,8 @@ import java.util.StringTokenizer;
 
 // 9184 신나는 함수 실행
 public class Main {
-    static int[][][] dp = new int[21][21][21];
+    static int[][][] dp = new int[101][101][101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -19,28 +20,25 @@ public class Main {
                 break;
             }
             sb.append("w(").append(a).append(", ").append(b).append(", ").append(c)
-                .append(") = ").append(w(a, b, c)).append("\n");
+                    .append(") = ").append(w(a + 50, b + 50, c + 50)).append("\n");
         }
         System.out.print(sb);
     }
 
     static int w(int a, int b, int c) {
-        if (a <= 0 || b <= 0 || c <= 0) {
-            return 1;
-        }
-        if (a > 20 || b > 20 || c > 20) {
-            return w(20, 20, 20);
-        }
-
         if (dp[a][b][c] > 0) {
             return dp[a][b][c];
         }
 
-        if (a < b && b < c) {
+        if (a <= 50 || b <= 50 || c <= 50) {
+            dp[a][b][c] = 1;
+        } else if (a > 70 || b > 70 || c > 70) {
+            dp[a][b][c] = w(70, 70, 70);
+        } else if (a < b && b < c) {
             dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-            return dp[a][b][c];
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
         }
-        dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
         return dp[a][b][c];
     }
 }
