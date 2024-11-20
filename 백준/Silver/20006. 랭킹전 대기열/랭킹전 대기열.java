354a6b5308429ad15a0ef8ec46c16956c89536e9
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-11-20 13:21:51 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 76 ms, Memory: 11948 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,71 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 // 20006 랭킹전 대기열
 public class Main {
-    static class Member {
-        int level;
-        String name;
-
-        public Member(int level, String name) {
-            this.level = level;
-            this.name = name;
-        }
-    }
     static class Room {
         int level;
-        List<Member> members; //방 인원
-        public Room(int level) {
+        TreeMap<String, Integer> map = new TreeMap<>(); //키값으로 정렬된 자료 구조
+        public Room(int level, String name) {
             this.level = level;
-            members = new ArrayList<>();
+            map.put(name, level);
         }
     }
-
-    static ArrayList<Room> room;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int p = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        room = new ArrayList<>();
-
+        ArrayList<Room> lists = new ArrayList<>();
         for (int i = 0; i < p; i++) {
             st = new StringTokenizer(br.readLine());
             int level = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            boolean isStartPossible = false;
+            boolean isFull = true;
 
-            for (Room current : room) {
-                if (10 >= Math.abs(current.level - level) && m > current.members.size()) {
-                    current.members.add(new Member(level, name));
-                    isStartPossible = true;
+            for (Room list : lists) {
+                if (list.map.size() != m && 10 >= Math.abs(level - list.level)) {
+                    list.map.put(name, level);
+                    isFull = false;
                     break;
                 }
             }
 
-            // 입장 가능한 방 없어 새로운 방 생성
-            if (!isStartPossible) {
-                Room next = new Room(level);
-                next.members.add(new Member(level, name));
-                room.add(next);
+            if (isFull) {
+                lists.add(new Room(level, name));
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (Room current : room) {
-            if (current.members.size() == m) {
-                sb.append("Started!").append("\n");
-            } else {
-                sb.append("Waiting!").append("\n");
-            }
+        for (Room room : lists) {
+            sb.append(room.map.size() == m ? "Started!" : "Waiting!").append("\n");
 
-            current.members.sort(Comparator.comparing(member -> member.name));
-            for (int j = 0; j < current.members.size(); j++) {
-                sb.append(current.members.get(j).level).append(" ")
-                        .append(current.members.get(j).name).append("\n");
+            for (Map.Entry<String, Integer> entry : room.map.entrySet()) {
+                sb.append(entry.getValue()).append(" ").append(entry.getKey()).append("\n");
             }
         }
         System.out.print(sb);
