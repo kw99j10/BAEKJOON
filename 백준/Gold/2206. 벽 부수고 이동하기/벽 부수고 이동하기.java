523c96a5f9ca3247474da426203db99a2bd4bd3f
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-12-04 19:31:51 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 624 ms, Memory: 113064 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -4,20 +4,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
-//벽 부수고 이동하기
+// 2206 벽 부수고 이동하기
 public class Main {
-    static int n, m;
+    static boolean[][][] visit;
+    static int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static int n, m, min = Integer.MAX_VALUE;
     static int[][] map;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int ans = Integer.MAX_VALUE; //최단 거리
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         map = new int[n][m];
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
@@ -25,50 +24,43 @@ public class Main {
                 map[i][j] = s.charAt(j) - '0';
             }
         }
-
-        //출발지와 도착지는 항상 0
         bfs();
-        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
     }
 
     static void bfs() {
         ArrayDeque<int[]> queue = new ArrayDeque<>();
-        boolean[][][] visit = new boolean[n][m][2];
+        visit = new boolean[n][m][2];
 
-        //0:벽을 부수지 않음, 1: 벽을 부숨
-        queue.add(new int[]{0, 0, 1, 0}); //좌표, 최단 경로, 벽을 부순지 여부
-        visit[0][0][0] = true;
+        queue.add(new int[]{0, 0, 0, 0}); //x,y,거리, 부숨 여부
+        visit[0][0][0] = true; // 벽 안 부숨:0, 벽 부숨: 1
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int x = current[0];
             int y = current[1];
-            int time = current[2];
-            int broken = current[3];
+            int distance = current[2];
+            int isBroken = current[3];
 
             if (x == n - 1 && y == m - 1) {
-                ans = Math.min(ans, time);
+                min = Math.min(min, distance + 1);
                 return;
             }
 
             for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+                int nx = x + move[d][0];
+                int ny = y + move[d][1];
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visit[nx][ny][isBroken]) {
                     continue;
                 }
 
-                if (visit[nx][ny][broken]) {
-                    continue;
-                }
-
-                if (map[nx][ny] == 0) {
-                    visit[nx][ny][broken] = true;
-                    queue.add(new int[]{nx, ny, time + 1, broken});
-                } else if (map[nx][ny] == 1 && broken == 0) {
+                if (isBroken == 0 && map[nx][ny] == 1) {
                     visit[nx][ny][1] = true;
-                    queue.add(new int[]{nx, ny, time + 1, 1});
+                    queue.add(new int[]{nx, ny, distance + 1, 1});
+                } else if (map[nx][ny] == 0) {
+                    visit[nx][ny][isBroken] = true;
+                    queue.add(new int[]{nx, ny, distance + 1, isBroken});
                 }
             }
         }
