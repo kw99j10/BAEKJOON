c2280b627cfc96a4b380926d0ff02ed016505efd
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2024-12-16 00:19:48 +0900

    [Silver V] Title: 중복된 숫자, Time: 1400 ms, Memory: 507080 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -1,25 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 15719 중복된 숫자
 public class Main {
-    static byte[] buffer = new byte[78888905];
-    static int next;
-    static int b;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    public static void main(String args[]) throws IOException {
-        System.in.read(buffer, 0, buffer.length);
-        long N = nextInt();
         long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(st.nextToken());
         }
-        System.out.println(sum - (N * (N - 1) / 2));
-    }
-
-    static long nextInt() {
-        long n = buffer[next++] - '0';
-        while ((b = buffer[next++]) >= '0')
-            n = (n * 10) + (b - '0');
-        return n;
+        System.out.println(sum - ((long) n * (n - 1)) / 2);
     }
 }
