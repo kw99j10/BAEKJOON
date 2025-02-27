d45e611841a130afd078ea5496d877c91b798dfc
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-02-27 09:43:38 +0900

    [Silver V] Title: 이름 궁합, Time: 268 ms, Memory: 90128 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.java
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -10,28 +10,28 @@ public class Main {
         String a = br.readLine();
         String b = br.readLine();
 
-        String tmp = "";
+        StringBuilder tmp = new StringBuilder();
         for (int i = 0; i < a.length(); i++) {
-            tmp += a.charAt(i);
-            tmp += b.charAt(i);
+            tmp.append(a.charAt(i));
+            tmp.append(b.charAt(i));
         }
 
-        String num = "";
+        StringBuilder num = new StringBuilder();
         for (int i = 0; i < tmp.length(); i++) {
-            num += alpha[tmp.charAt(i) - 65];
+            num.append(alpha[tmp.charAt(i) - 65]);
         }
 
         while (num.length() > 2) {
 
-            tmp = "";
+            tmp = new StringBuilder();
             for (int i = 0; i < num.length() - 1; i++) {
                 int n1 = num.charAt(i) - '0';
                 int n2 = num.charAt(i + 1) - '0';
                 int answer = (n1 + n2) % 10;
-                tmp += answer;
+                tmp.append(answer);
             }
-            num = tmp;
+            num = new StringBuilder(tmp.toString());
         }
-        System.out.println(num.length() == 1 ? "0" + num : num);
+        System.out.println(num.length() == 1 ? "0" + num : num.toString());
     }
 }
