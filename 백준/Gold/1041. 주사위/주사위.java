df9578e7660603de3428d3d094ac13bf6ef2e14a
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-03-11 09:18:46 +0900

    [Gold V] Title: 주사위, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Gold/1041. 주사위/주사위.java
+++ 백준/Gold/1041. 주사위/주사위.java
@@ -10,44 +10,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] dice = new int[6];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int tSum = 0;
         int max = 0;
+        long sum = 0L;
         for (int i = 0; i < 6; i++) {
             dice[i] = Integer.parseInt(st.nextToken());
-            tSum += dice[i];
             max = Math.max(max, dice[i]);
+            sum += dice[i];
         }
 
         if (n == 1) {
-            System.out.println(tSum - max);
-            return; // 1인 경우 제외
+            System.out.println(sum - max);
+            return;
         }
 
         int[] min = new int[3];
         for (int i = 0; i < 3; i++) {
             min[i] = Math.min(dice[i], dice[5 - i]);
         }
-
-        // 윗면 기준
-        // 1. 3면이 보이는 주사위는 4개
-        long sum = 0L;
-        for (int i = 0; i < 3; i++) {
-            sum += min[i];
-        }
-        sum *= 4;
-
-
-        // 2. 2면이 보이는 주사위
         Arrays.sort(min);
-        sum += (long) ((n - 2) * 8 + 4) * (min[0] + min[1]);
-        sum += (long) (n - 2) * 4 * min[0];
 
-
-        // 3. 1면만 보이는 주사위
-        sum += (long) Math.pow(n - 2, 2) * 5 * min[0];
-        System.out.println(sum);
+        // 한 면만 보이는 경우
+        long one = (long) (Math.pow(n - 2, 2) + (long) (n - 2) * (n - 1) * 4);
+        long two = (n - 2) * 4L + (n - 1) * 4L;
+        long three = (min[0] + min[1] + min[2]); // 꼭짓점 4개
+        System.out.println(one * min[0] + two * (min[0] + min[1]) + three * 4);
     }
 }
