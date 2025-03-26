0d508a7b7f2ee918a5cfb32050dfe89986b388c1
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-03-26 10:35:33 +0900

    [Gold IV] Title: 무기 공학, Time: 580 ms, Memory: 17348 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.java
+++ 백준/Gold/18430. 무기 공학/무기 공학.java
@@ -5,13 +5,10 @@ import java.util.StringTokenizer;
 
 // 18430 무기 공학
 public class Main {
+    static int[][] move = {{0, -1, 1, 0}, {0, -1, -1, 0}, {0, 1, -1, 0}, {0, 1, 1, 0}};
     static int n, m, max;
     static int[][] grid;
     static boolean[][] visit;
-
-    // 부메랑 기준
-    static int[][] move = {{-1, 0, 0, -1}, {-1, 0, 0, 1}, {1, 0, 0, -1}, {1, 0, 0, 1}};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,45 +23,44 @@ public class Main {
         }
 
         if (n == 1 || m == 1) {
-            System.out.println(0); // 부메랑을 만들 수 없음
+            System.out.println(0); // 부메랑을 만들 수 없는 경우
             return;
         }
 
         visit = new boolean[n][m];
-        dfs(0, 0, 0);
+        dfs(0, 0, 0); // 0,0 부터 n-1,m-1 까지 완전 탐색
         System.out.println(max);
     }
 
     static void dfs(int x, int y, int sum) {
-
         if (x == n) {
-            max = Math.max(max, sum); //완탐
+            max = Math.max(max, sum);
             return;
         }
 
         if (y == m) {
             x++;
             y = 0;
+            dfs(x, y, sum);
         }
 
         for (int d = 0; d < 4; d++) {
-            int nx = x + move[d][0];
-            int ny = y + move[d][1];
-            int kx = x + move[d][2];
-            int ky = y + move[d][3];
-
-            if (isPossible(nx, ny) || isPossible(kx, ky) || visit[x][y] || visit[nx][ny] || visit[kx][ky]) {
-                continue;
+            int nx = x + move[d][0]; int ny = y + move[d][1];
+            int kx = x + move[d][2]; int ky = y + move[d][3];
+            if (isPossible(nx, ny) && isPossible(kx, ky) && !visit[x][y]) {
+                visit[x][y] = true;
+                visit[nx][ny] = true;
+                visit[kx][ky] = true;
+                dfs(x, y + 1, sum + grid[x][y] * 2 + grid[nx][ny] + grid[kx][ky]);
+                visit[x][y] = false;
+                visit[nx][ny] = false;
+                visit[kx][ky] = false;
             }
-
-            visit[x][y] = visit[nx][ny] = visit[kx][ky] = true;
-            dfs(x, y + 1, sum + grid[x][y] * 2 + grid[nx][ny] + grid[kx][ky]);
-            visit[x][y] = visit[nx][ny] = visit[kx][ky] = false;
         }
-        dfs(x, y + 1, sum); // 부메랑 x
+        dfs(x, y + 1, sum); // 부메랑을 만들지 않는 경우
     }
 
     static boolean isPossible(int x, int y) {
-        return x < 0 || x >= n || y < 0 || y >= m;
+        return x >= 0 && x < n && y >= 0 && y < m && !visit[x][y];
     }
 }
