37e7033e27ac8210ed20c9c23f83a070866f2f8a
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-04-26 15:27:16 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+// 1477 휴게소 세우기
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.add(0);
+        list.add(l); // 0 ~ l 까지의 휴게소
+        Collections.sort(list);
+
+        int start = 1;
+        int end = l - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2; // 간격의 기준
+
+            int count = 0;
+            for (int i = 1; i < list.size(); i++) {
+                int d = list.get(i) - list.get(i - 1);
+                count += d / mid;
+                if (d % mid == 0) {
+                    count--; // 중복 방지
+                }
+            }
+
+            if (m >= count) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println(start);
+    }
+}
