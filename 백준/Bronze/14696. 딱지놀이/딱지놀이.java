f103f6263c40b38033a51704cc7edf18f5ef88c6
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-05-20 17:53:42 +0900

    [Bronze I] Title: 딱지놀이, Time: 244 ms, Memory: 28632 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.java
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 // 14696 딱지놀이
@@ -14,56 +12,33 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            ArrayList<Integer> a = new ArrayList<>();
-            ArrayList<Integer> b = new ArrayList<>();
+            int[] a = new int[4];
+            int[] b = new int[4];
 
             st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
             for (int j = 0; j < num; j++) {
-                a.add(Integer.parseInt(st.nextToken()));
+                a[Integer.parseInt(st.nextToken()) - 1]++;
             }
 
             st = new StringTokenizer(br.readLine());
             int num2 = Integer.parseInt(st.nextToken());
             for (int j = 0; j < num2; j++) {
-                b.add(Integer.parseInt(st.nextToken()));
+                b[Integer.parseInt(st.nextToken()) - 1]++;
             }
 
-            if (a.size() > b.size()) {
-                for (int j = 0; j < a.size() - b.size(); j++) {
-                    b.add(0);
-                }
-            } else if (b.size() > a.size()) {
-                for (int j = 0; j < b.size() - a.size(); j++) {
-                    a.add(0);
-                }
-            }
-
-            Collections.sort(a);
-            Collections.sort(b);
-
-            boolean isA = false;
-            boolean isB = false;
-            for (int j = a.size() - 1; j >= 0; j--) {
-                int ansA = a.get(j);
-                int ansB = b.get(j);
-
-                if (ansA > ansB) {
-                    isA = true;
-                    break;
-                } else if (ansB > ansA) {
-                    isB = true;
-                    break;
-                }
-            }
-
-            if (isA) {
-                sb.append("A").append('\n');
-            } else if (isB) {
-                sb.append("B").append('\n');
+            if (a[3] != b[3]) {
+                sb.append(a[3] > b[3] ? "A" : "B");
+            } else if (a[2] != b[2]) {
+                sb.append(a[2] > b[2] ? "A" : "B");
+            } else if (a[1] != b[1]) {
+                sb.append(a[1] > b[1] ? "A" : "B");
+            } else if (a[0] != b[0]) {
+                sb.append(a[0] > b[0] ? "A" : "B");
             } else {
-                sb.append("D").append('\n');
+                sb.append("D");
             }
+            sb.append("\n");
         }
         System.out.print(sb);
     }
