fdac13501b89ec1851487e529236b45f555f5c69
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-05-26 20:59:06 +0900

    [Gold IV] Title: 호석이 두 마리 치킨, Time: 244 ms, Memory: 17192 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -4,55 +4,52 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//호석이 두 마리 치킨
+// 21278 호석이 두 마리 치킨
 public class Main {
-    static final int INF = 999999;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] road = new int[n + 1][n + 1];
+        int[][] grid = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            Arrays.fill(road[i], INF);
+            Arrays.fill(grid[i], 999999);
+            grid[i][i] = 0;
         }
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            road[a][b] = 1;
-            road[b][a] = 1;
+            grid[a][b] = 1;
+            grid[b][a] = 1;
         }
 
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (i == j) {
-                        road[i][j] = 0;
-                    }
-                    road[i][j] = Math.min(road[i][j], road[i][k] + road[k][j]);
+                    grid[i][j] = Math.min(grid[i][j], grid[i][k] + grid[k][j]);
                 }
             }
         }
 
-        int idx1 = 0;
-        int idx2 = 0;
+        int n1 = 0, n2 = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 int sum = 0;
                 for (int k = 1; k <= n; k++) {
-                    sum += Math.min(road[i][k] + road[k][i], road[j][k] + road[k][j]);
-                }
+                    sum += Math.min(grid[i][k] + grid[k][i], grid[j][k] + grid[k][j]);
+                } // 왕복 시간
+
                 if (min > sum) {
-                    idx1 = i;
-                    idx2 = j;
+                    n1 = i;
+                    n2 = j;
                     min = sum;
                 }
             }
         }
-        System.out.println(idx1 + " " + idx2 + " " + min);
+        System.out.println(n1 + " " + n2 + " " + min);
     }
 }
