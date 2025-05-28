15f5385241f22e5c721d9dd16e70d4c65a18194e
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2025-05-28 17:09:15 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 108 ms, Memory: 14296 KB, Score: 18 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -9,22 +9,22 @@ import java.util.StringTokenizer;
 // 20168 골목대장 호석
 public class Main {
     static class Node implements Comparable<Node> {
-        int node, price;
+        int node, max, total;
 
-        public Node(int node, int price) {
+        public Node(int node, int max, int total) {
             this.node = node;
-            this.price = price;
+            this.max = max;
+            this.total = total;
         }
 
         @Override
         public int compareTo(Node o) {
-            return o.price - this.price;
+            return this.max - o.max; // 최대 요금을 최소화
         }
     }
 
-    static int n, m, a, b, c, min = 0;
+    static int n, m, a, b, c, min = Integer.MAX_VALUE;
     static ArrayList<Node>[] lists;
-    static int[] distance;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,10 +36,8 @@ public class Main {
         c = Integer.parseInt(st.nextToken());
 
         lists = new ArrayList[n + 1];
-        distance = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             lists[i] = new ArrayList<>();
-            distance[i] = Integer.MAX_VALUE;
         }
 
         for (int i = 0; i < m; i++) {
@@ -47,40 +45,37 @@ public class Main {
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
-            lists[s].add(new Node(e, p));
-            lists[e].add(new Node(s, p));
+            lists[s].add(new Node(e, p, p));
+            lists[e].add(new Node(s, p, p));
         }
 
         dijkstra(a, b, c);
-        System.out.println(distance[b] == Integer.MAX_VALUE ? -1 : min);
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
     }
 
     static void dijkstra(int start, int end, int cut) {
         PriorityQueue<Node> queue = new PriorityQueue<>();
         boolean[] visit = new boolean[n + 1];
-        queue.add(new Node(start, 0));
-        visit[start] = true;
+        queue.add(new Node(start, 0, 0));
         while (!queue.isEmpty()) {
             Node current = queue.poll();
 
             if (current.node == end) {
+                min = Math.min(min, current.max);
                 break;
             }
 
-            if (!visit[current.node]) {
-                visit[current.node] = true;
+            if (visit[current.node]) {
+                continue;
             }
+            visit[current.node] = true;
 
             for (Node next : lists[current.node]) {
-                int price = current.price + next.price;
-                if (price > cut) {
-                    continue;
-                }
+                int nextSum = current.total + next.total;
+                int max = Math.max(current.max, next.total);
 
-                if (distance[next.node] > current.price + next.price) {
-                    distance[next.node] = current.price + next.price;
-                    min = Math.max(min, next.price);
-                    queue.add(new Node(next.node, distance[next.node]));
+                if (cut >= nextSum) {
+                    queue.add(new Node(next.node, max, nextSum));
                 }
             }
         }
